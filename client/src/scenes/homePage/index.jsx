cf50c24872af1de661c0896b0462759dfe9b720f
Aniket <113533605+sweets-11@users.noreply.github.com>
2023-02-12 23:38:12 +0530

    initial commit

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,83 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import Navbar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
+
+const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+  // sx={{position:"fixed" , top:"010"}}
+  return (
+    <Box>
+      <Navbar />
+      {isNonMobileScreens ? (
+        <Box
+          width="100%"
+          padding="2rem 6%"
+          display={isNonMobileScreens ? "flex" : "block"}
+          gap="4.5rem"
+          sx={{ scrollBehavior: "smooth" }}
+        >
+          {/* personal profile */}
+          <Box>
+            <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+              <UserWidget userId={_id} picturePath={picturePath} />
+            </Box>
+          </Box>
+
+          {/* Posts */}
+          <Box
+            flexBasis={isNonMobileScreens ? "32%" : undefined}
+            mt={isNonMobileScreens ? undefined : "2rem"}
+          >
+            <PostsWidget userId={_id} />
+          </Box>
+          {/* </Box> */}
+
+          <Box
+            flexBasis={isNonMobileScreens ? "31%" : undefined}
+            mt={isNonMobileScreens ? undefined : "2rem"}
+          >
+            <MyPostWidget picturePath={picturePath} />
+          </Box>
+        </Box>
+      ) : (
+        <Box
+          width="100%"
+          padding="2rem 6%"
+          display={isNonMobileScreens ? "flex" : "block"}
+          gap="4.5rem"
+          sx={{ scrollBehavior: "smooth" }}
+        >
+          {/* personal profile */}
+          <Box>
+            <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+              <UserWidget userId={_id} picturePath={picturePath} />
+            </Box>
+          </Box>
+
+          <Box
+            flexBasis={isNonMobileScreens ? "26%" : undefined}
+            mt={isNonMobileScreens ? undefined : "2rem"}
+          >
+            <MyPostWidget picturePath={picturePath} />
+          </Box>
+
+          <Box
+            flexBasis={isNonMobileScreens ? "32%" : undefined}
+            mt={isNonMobileScreens ? undefined : "2rem"}
+          >
+            <PostsWidget userId={_id} />
+          </Box>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default HomePage;
+
+// on scroll how to make something fixed at one place
